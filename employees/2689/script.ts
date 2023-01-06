6117e8725ea8aff76d62b251410273c52948719a
Xsquared Labs <104787509+XsqrdLabsAdmin@users.noreply.github.com>
2023-01-06 15:27:16 -0500

    Add files via upload

---
+++ employees/2689/script.ts
@@ -0,0 +1,26 @@
+import GlitchedWriter, {
+    wait
+  } from "https://cdn.skypack.dev/glitched-writer@2.0.29";
+  
+  // Glitched Writer npm module:
+  // https://www.npmjs.com/package/glitched-writer
+  
+  const Writer = new GlitchedWriter('#glitch_this', { letterize: true }, logString);
+  
+  (async  ()=> {
+    await wait(1000);
+    await Writer.write("Good Bye!");
+    input.removeAttribute("disabled");
+  })();
+  
+  function logString(string) {
+    logs.innerHTML += `<p>${string}</p>`;
+  }
+  
+  input.addEventListener(
+    "input",
+    _.debounce(() => 
+      Writer.write(input.value)
+    , 500)
+  );
+  
